d55b47f9c2b7a57337480b6c2c1f1420021fd966
Mayurp375 <mayurpolojwar1234@gmail.com>
2023-08-12 00:34:47 +0530

    ability to sent otp in mail

--- src/main/java/com/loan/service/register/RegisterService.java
+++ src/main/java/com/loan/service/register/RegisterService.java
@@ -3,6 +3,7 @@ package com.loan.service.register;
 import com.loan.entity.User.User;
 import com.loan.repository.register.RegistrationRepo;
 import com.loan.service.mailsender.Mail;
+import com.loan.service.mailsender.MailSendere;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,6 +21,9 @@ public class RegisterService implements RegistrationInterface {
 
     @Override
     public User register(User user) {
+        mail.sendSimpleEmail(user.getEmail(),MailSendere.generateOtp(),
+                "Your requested One time password ");
+        log.info("OTP is sent Successfully");
         return registrationRepo.save(user);
     }
 
